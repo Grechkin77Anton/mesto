682e6dd0e03462df65265488dd73d4755d6585f4
Anton Grechkin <grechkinanton70@yandex.ru>
2023-03-09 19:09:28 +0700

    fix: Исправить JS, разметку, CSS

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,23 @@
 // Находим форму в DOM
 let popup = document.querySelector('.popup');
 let formElement = document.querySelector('.popup__container'); // Воспользуйтесь методом querySelector()
-let nameInput = document.querySelector('.popup__author'); // Воспользуйтесь инструментом .querySelector()
-let jobInput = document.querySelector('.popup__description');// Воспользуйтесь инструментом .querySelector()
+let nameInput = document.querySelector('#title'); // Воспользуйтесь инструментом .querySelector()
+let jobInput = document.querySelector('#description');// Воспользуйтесь инструментом .querySelector()
 let nameAuthor = document.querySelector('.profile-info__author');
 let jobAuthor = document.querySelector('.profile-info__description');
-let formSubmit = document.querySelector('.popup__button-save');
 let editButton = document.querySelector('.profile__edit-button');
 let addButton  = document.querySelector('.profile__add-button');
-let reserEditButton = document.querySelector('.popup__close');
+let resetEditButton = document.querySelector('.popup__close');
 
+function hidePopup() {
+    popup.classList.remove('popup_opened');
+}
+
+function showPopup() {
+    popup.classList.add('popup_opened');
+    nameInput.value = nameAuthor.textContent;
+    jobInput.value = jobAuthor.textContent;
+}
 
 function handleFormSubmit (evt) {
     evt.preventDefault();
@@ -21,16 +29,5 @@ function handleFormSubmit (evt) {
 }
 
 formElement.addEventListener('submit',handleFormSubmit);
-
-function showPopup() {
-    popup.classList.add('popup__opened');
-    nameInput.value = nameAuthor.textContent;
-    jobInput.value = jobAuthor.textContent;
-}
-
-function hidePopup() {
-    popup.classList.remove('popup__opened');
-}
-
 editButton.addEventListener('click',showPopup);
-reserEditButton.addEventListener('click',hidePopup);
+resetEditButton.addEventListener('click',hidePopup);
