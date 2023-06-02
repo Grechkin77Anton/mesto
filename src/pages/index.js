c0ca1ab66f5dcad7516c87b4752a8acd824321f7
Anton Grechkin <grechkinanton70@yandex.ru>
2023-06-02 16:55:51 +0700

    fix: display page

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import UserInfo from '../components/userInfo.js';
 import PopupWithForm from '../components/PopupWithForm.js';
 import PopupDeleteCard from '../components/PopupDeleteCard.js';
 import Api from '../components/Api.js';
-// import './index.css';
+import './index.css';
 
 const editButton = document.querySelector('.profile__edit-button');
 const addButton  = document.querySelector('.profile__add-button');
